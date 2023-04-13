81a7e5694b00ea6cf6cb327fb7f7f5be75900e78
Gwenitora <gwenitora.gardu74@gmail.com>
2023-04-13 14:51:23 +0200

    Debug: message of universal parse json

--- dependencies/parsejson/parse.py
+++ dependencies/parsejson/parse.py
@@ -3,7 +3,9 @@ import os
 
 ASSETS = {}
 
-def parseJson(path):
+def parseJson(path: str) -> False | list | dict:
+    """Parse le fichier json au chemin projet/`path` sous forme de liste ou de dictionnaire ou renvoie False si le fichier n'est pas trouver.\n
+    Inclue la variable `ASSETS` déjà parser pour récupérer les assets du projet"""
     global ASSETS
     try:
         file = open(os.getcwd()+path, 'r')
