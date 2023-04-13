5d09541182f1df510d60e22b27556b64e44ce1dc
Gwenitora <gwenitora.gardu74@gmail.com>
2023-04-13 15:46:11 +0200

    Feature: fps in cmd

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 # Description: Main du projetdependencies/moderngl
 def main():
     app = loadgl.GraphicsEngine()
+    timing = time.time()
     while True:
         app.get_time()
         app.check_events()
         app.camera.update()
         app.render()
         app.delta_time = app.clock.tick(60)
+        print("FPS: " + str(int(60 / (time.time() - timing))))
+        timing = time.time()
 
 #################################################
 
 # Import des dependances
 from dependencies.parsejson.parse import *
 import dependencies.moderngl.main as loadgl
+import time
 
 # Check si le projet se lance avec succes
 print("Start with sucess")
