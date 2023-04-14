e7d463817a8bdb153e92bd96205e80487f42f275
Lenny Tournier <ltournier@gaming.tech>
2023-04-14 12:43:31 +0200

    Update pool

--- dependencies/moderngl/pool_handling.py
+++ dependencies/moderngl/pool_handling.py
@@ -4,10 +4,11 @@ class pool:
     def __init__(self):
         self.f = []
 
-
     def Pull(self, x):
         self.f.remove(x)
 
-
     def Push(self, y):
         self.f.append(y)
+
+    def clear(self):
+        self.f.clear()
