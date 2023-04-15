e912ad1683bf76ecbe8b8597bfde8c03dab1cd24
Arno Labourdette <alabourdette@gaming.tech>
2023-04-15 18:07:37 +0200

    Debug: cam / smooth rotation

--- dependencies/engine/gameobject.py
+++ dependencies/engine/gameobject.py
@@ -9,13 +9,7 @@ from dependencies.moderngl.model import ExtendedBaseModel
 class GameObject(ExtendedBaseModel):
     def __init__(self, pos = (0, 0, 0), rot = (0, 0, 0), scale = (1, 1, 1)):
         self.position = glm.vec3(pos)
-        self.prev_position = self.position
-        self.camera_pos = self.position + glm.vec3([0, 0.2, 0.5])
         self.rotation = glm.vec3(rot)
-        self.prev_rotation = self.rotation
-        self.camera_yaw = self.rotation[1] - 90
-        self.camera_pitch = self.rotation[0] - 10
-        self.camera_roll = self.rotation[2]
         self.scale = scale
         self.UID = "-1"
 
@@ -43,61 +37,21 @@ class GameObject(ExtendedBaseModel):
 
 
     # Utiliser cette fonction pour avoir les collision
-    def Move(self, translation: tuple, timing: int = 0, camTranslation: tuple = glm.vec3([0, 0, 0])) -> None:
+    def Move(self, translation: tuple) -> None:
         """Fonction pour déplacer l'objet et permettre les collision\n
         translation: (x, y, z) déplacement de l'objet"""
-        self.prev_position += translation
+        self.position += translation
         self.velocity = translation
-        self.camera_pos += camTranslation
     
-    # Utiliser cette fonction pour avoir la rotation
-    def Rotate(self, rotation: tuple, camRotation: tuple = (0, 0, 0)) -> None:
-        """Fonction pour tourner l'objet\n
-        rotation: (x, y, z) rotation de l'objet"""
-        self.prev_rotation += rotation
-        self.camera_yaw += camRotation[0]
-        self.camera_pitch += camRotation[1]
-        self.camera_roll += camRotation[2]
-        
-    # Utiliser cette fonction pour avoir les collision
-    def SetPos(self, position: tuple, camPosition: tuple = glm.vec3([0, 0.15, 0.2])) -> None:
-        """Fonction pour définir un position de l'objet\n
-        translation: (x, y, z) position de l'objet"""
-        self.prev_position = position
-        self.camera_pos = camPosition
-    
-    # Utiliser cette fonction pour avoir la rotation
-    def SetRot(self, orientation: tuple, camOrientation: tuple = (-10, 90, 0)) -> None:
+    # Utiliser cette fonction pour définir la rotation
+    def Rotate(self, angle: float, axis: tuple) -> None:
         """Fonction pour définir une rotation de l'objet\n
         orientation: (x, y, z) orientation de l'objet"""
-        self.rotation = orientation
-        self.camera_pitch = camOrientation[0]
-        self.camera_yaw = camOrientation[1]
-        self.camera_roll = camOrientation[2]
-    
-    def SyncPosCamera(self, position: tuple = glm.vec3([0, 0.2, -0.5])) -> None:
-        self.camera_pos = glm.vec3(self.position) + position
-    
-    def SyncRotCamera(self, camOrientation: tuple = (-10, 90, 0)) -> None:
-        self.camera_pitch = self.rotation[1]*10 + camOrientation[0]
-        self.camera_yaw = self.rotation[0]*10 + camOrientation[1]
-        self.camera_roll = self.rotation[2]*10 + camOrientation[2]
+        self.rotation += angle * axis
     
     def Update(self):
-        if Eng.Engine.Instance.deltaTime > 0:
-            self.move_position = self.position - self.prev_position
-            if math.sqrt(self.move_position.x**2 + self.move_position.y**2 + self.move_position.z**2) <= 5:
-                self.position = self.prev_position
-            else:
-                self.position -= self.move_position * 0.5
-        self.model.pos = self.position
-
-        if Eng.Engine.Instance.deltaTime > 0:
-            self.move_rotation = self.rotation - self.prev_rotation
-            if math.sqrt(self.move_rotation.x**2 + self.move_rotation.y**2 + self.move_rotation.z**2) <= 0.01:
-                self.rotation = self.prev_rotation
-            else:
-                self.rotation -= self.move_rotation * 0.5
-        self.model.rot = self.rotation
         
-        if(self.model != NULL) : self.model.m_model = self.model.get_model_matrix()
+        if(self.model != NULL) : 
+            self.model.pos = self.position
+            self.model.rot = self.rotation
+            self.model.m_model = self.model.get_model_matrix()
--- dependencies/scripts/entities/player.py
+++ dependencies/scripts/entities/player.py
@@ -7,13 +7,21 @@ class Player(GameObject):
     def __init__(self, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
         self.vue = 3
         self.vue_pressed = 0
+        self.cameraOffset = glm.vec3([0, 0.2, -0.5])
         self.right = glm.vec3(1, 0, 0)
         self.up = glm.vec3(0, 1, 0)
         self.forward = glm.vec3(0, 0, 1)
         self.speed = 0.01
+        self.rotSpeed = 0.01
         self.scrollSpeed = 0.03
         super().__init__(pos, rot, scale)
         eng.Engine.Instance.graphicEngine.camera.target = self
+
+    def SetRotCamera(self, camOrientation: tuple = (-10, 0, 0)) -> None:
+        cam = Eng.Engine.Instance.graphicEngine.camera
+        cam.pitch = self.rotation[1]#*10 + camOrientation[0]
+        cam.yaw = self.rotation[0]#*10 + camOrientation[1]
+        cam.roll = self.rotation[2]#*10 + camOrientation[2]
 
     def OnCollide(self, colider):
         print("["+pg.time.get_ticks().__str__()+"] Lost")
@@ -24,34 +32,53 @@ class Player(GameObject):
         rotX = 0
         rotY = 0
         rotZ = 0
+        #Position
         if keys[pg.K_z]:
             self.Move(self.up * self.speed * eng.Engine.Instance.deltaTime)
-            rotX = 0.4
+            rotX = 1
         if keys[pg.K_s]:
             self.Move(-self.up * self.speed * eng.Engine.Instance.deltaTime)
-            rotX = -0.4
+            rotX = -1
         if keys[pg.K_q]:
             self.Move(-self.right * self.speed * eng.Engine.Instance.deltaTime)
-            rotZ = 0.4
+            rotZ = 1
         if keys[pg.K_d]:
             self.Move(self.right * self.speed * eng.Engine.Instance.deltaTime)
-            rotZ = -0.4
+            rotZ = -1
+        #Caméra
         if keys[pg.K_e] and self.vue_pressed == 0:
-            self.vue += 2
-            self.vue %= 4
+            self.camera_offset = glm.vec3([0, 0, 0])
             self.vue_pressed = 1
         elif not(keys[pg.K_e]) and self.vue_pressed == 1:
+            self.camera_offset = glm.vec3([0, 0.2, -0.5])
+            self.SetRotCamera()
             self.vue_pressed = 0
+        #Cheat code start
         if keys[pg.K_a] and self.vue_pressed == 0:
-            self.SetPos(glm.vec3([0, 0, 0]))
+            self.position = glm.vec3([0, 0, 0])
         else:
+        #Cheat code end
             self.Move(self.forward * self.scrollSpeed * eng.Engine.Instance.deltaTime)
-            self.SetRot(glm.vec3([rotX, rotY, rotZ]))
+            
+            maxAngle = math.pi / 4
+            if(self.rotation[0] > maxAngle or self.rotation[0] < -maxAngle) : rotX = 0
+            if(self.rotation[2] > maxAngle or self.rotation[2] < -maxAngle) : rotZ = 0
+            
+            if(rotX == 0 and (keys[pg.K_z] or keys[pg.K_s]) == False):
+                if(self.rotation[0] > 0.08) : rotX = -1 
+                elif(self.rotation[0] > 0) : self.rotation[0] = 0
+                elif(self.rotation[0] < -0.08) : rotX = 1
+                else : self.rotation[0] = self.rotation[0] = 0
+            if(rotZ == 0 and (keys[pg.K_q] or keys[pg.K_d]) == False):
+                if(self.rotation[2] > 0.08) : rotZ = -1
+                elif(self.rotation[2] > 0) : self.rotation[2] = 0
+                elif(self.rotation[2] < -0.08) : rotZ = 1
+                else : self.rotation[2] = 0
 
-        if self.vue == 3:
-            self.SyncPosCamera()
-        elif self.vue == 1:
-            self.SyncPosCamera(glm.vec3([0, 0.0, -0.1]))
-            self.SyncRotCamera((-90, 0, 0))
+            self.Rotate(self.rotSpeed * eng.Engine.Instance.deltaTime, glm.vec3([rotX, rotY, rotZ]))
+
+        Eng.Engine.Instance.graphicEngine.camera.position = self.position + self.cameraOffset
+        if self.vue == 1 :
+            self.SetRotCamera((self.rotation[0], self.rotation[1], self.rotation[2]))
 
         super().Update()
