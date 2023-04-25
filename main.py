04c664ff2153077066ff98de5e5bbaebcbfe0d12
Gwenitora <gwenitora.gardu74@gmail.com>
2023-04-25 21:01:52 +0200

    Update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     Engine.CreateInstance()
     Engine.Instance.LoadScene("test")
+
+    Playlist.CreateInstance(Engine.Instance.MasterVolume)
+    Playlist.Instance.miscs["game"].play()
     
     Engine.Instance.Start()
 
@@ -11,6 +14,7 @@ from dependencies.parsejson.parse import *
 import dependencies.moderngl.main as loadgl
 from dependencies.engine.engine import *
 from dependencies.engine.gameobject import *
+from dependencies.music.music_control import Playlist
 
 # Check si le projet se lance avec succes
 print("Start with sucess")
