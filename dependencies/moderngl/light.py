280e5ecf3fa4025e8ba9e86d57a7a0e47705f293
Arno Labourdette <117281265+ArnoLabourdette@users.noreply.github.com>
2023-04-26 16:15:04 +0200

    Update: light

--- dependencies/moderngl/light.py
+++ dependencies/moderngl/light.py
@@ -4,9 +4,9 @@ class Light:
     def __init__(self, position=(50, 50, -10), color=(1, 1, 1)):
         self.position = glm.vec3(position)
         self.color = glm.vec3(color)
-        self.direction = glm.vec3(0, 0, 0)
+        self.direction = glm.vec3(0, -0.75, 0.45)
         # intensities
-        self.Ia = 0.06 * self.color  # ambient
+        self.Ia = 0.3 * self.color  # ambient
         self.Id = 0.8 * self.color  # diffuse
         self.Is = 1.0 * self.color  # specular
         # view matrix
